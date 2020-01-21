c34901623419f8bd190c91d5ba396fb14e97bbbe
Ran Regev <regev.ran@gmail.com>
2020-01-21 13:14:39 +0200

    Add files via upload

---
+++ maintenance/if_statement.cpp
@@ -0,0 +1,28 @@
+﻿
+struct A { bool f(); };
+
+int f1( A* pa )
+{
+    if ( pa )
+    {
+        if ( pa->f() )
+        {
+            // more code
+        }
+    }
+    return 0;
+}
+
+int f2( A* pa )
+{
+    if ( pa && pa->f() )
+    {
+        // more code
+    }
+    return 0;
+}
+
+int main()
+{
+    return 0;
+}
