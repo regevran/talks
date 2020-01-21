918ca22a3871b760884d837780817545d6fd5004
Ran Regev <regev.ran@gmail.com>
2020-01-21 12:55:02 +0200

    maintenenace ex1

---
+++ Compiler Explorer C++ Editor #[object Object] Code (1).cpp
@@ -0,0 +1,25 @@
+﻿#include <vector>
+#include <iostream>
+#include <numeric>
+
+int prevSum()
+{
+    return 7;
+}
+
+int main()
+{
+    std::vector v = {1,2,3,4,5,6,7,8,9,10};
+
+    int sum = prevSum();
+    for ( int i = 0; i < v.size(); ++ i )
+    {
+        sum += v[i];
+    }
+
+    std::cout << sum << "\n";
+
+    int sum1 = std::accumulate( v.begin(), v.end(), prevSum() );
+
+    std::cout << sum1 << "\n";
+}
