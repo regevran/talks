b4f008192be864a0f0d2b7940171cde8c4d1fce6
Ran Regev <ran.regev@scylladb.com>
2025-02-28 22:26:36 +0200

    exception croses noexecpt

--- contracts_pitfalls/test_contracts/test.cpp
+++ contracts_pitfalls/test_contracts/test.cpp
@@ -3,32 +3,27 @@
 #define BOOST_TEST_MODULE My Test 
 #include <boost/test/included/unit_test.hpp> 
 
-bool mitigation_1(int min, int max, int x) {
-    return x > min and x < max;
-}
-
 void the_function_to_test_its_contracts(int x) noexcept
     pre(x > 0)
     pre(x < 10) {
-//    std::cout << "body of tested function\n";
-}
 
-BOOST_AUTO_TEST_CASE(mitigation_test) 
-{
-    BOOST_CHECK(
-        not mitigation_1(0, 10, 11)
-    );
-}
+    // when the body of this function 
+    // is empty, the result is:
+    // the thrown exception from the handler _crosses_
+    // the noexcpt and propegates to the caller
+    // (thus the test passes)
 
+    // std::cout << "body of tested function\n";
+}
 
 BOOST_AUTO_TEST_CASE(first_test) 
 {
     BOOST_CHECK_THROW(
         the_function_to_test_its_contracts(11),
         std::logic_error
+        //std::bad_alloc
     );
 }
-
 
 BOOST_AUTO_TEST_CASE(second_test) 
 {
@@ -37,4 +32,15 @@ BOOST_AUTO_TEST_CASE(second_test)
     );
 }
 
+bool mitigation_1(int min, int max, int x) {
+    return x > min and x < max;
+}
+
+
+BOOST_AUTO_TEST_CASE(mitigation_test) 
+{
+    BOOST_CHECK(
+        not mitigation_1(0, 10, 11)
+    );
+}
 
